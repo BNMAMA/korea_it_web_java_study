c92ef3490cbcae9ebcd3de27f7a3e7c71105df7f
BNMAMA <ksuyeon56@naver.com>
2025-05-19 21:48:01 +0900

    Feat:이중반복문, 구구단 만들기

---
+++ src/_12_MultipleTable/MultipleTable.java
@@ -0,0 +1,15 @@
+package _12_MultipleTable;
+
+public class MultipleTable {
+    public static void main(String[] args) {
+        //1 ~ 9단
+        for (int i = 1; i <= 9 ; i++) {
+            System.out.println(i + "단");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + "x" + j + "=" + i * j);
+
+            }
+
+        }
+    }
+}
